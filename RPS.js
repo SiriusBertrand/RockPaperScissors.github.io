69117a883624ae35ed25691c023ac25144d805c7
SiriusBertrand <cedric.dzameshie@protonmail.com>
2023-06-26 21:47:43 -0500

    On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   RPS.css
    	modified:   RPS.js
    	modified:   index.html

--- RPS.js
+++ RPS.js
@@ -1,134 +1,129 @@
-// Game logic and variables
-let userScore = 0;
+// Global variables for keeping track of scores and rounds
+let playerScore = 0;
 let computerScore = 0;
+let currentRound = 0;
+const totalRounds = 3;
 
-const rockButton = document.getElementById("rock");
-const paperButton = document.getElementById("paper");
-const scissorsButton = document.getElementById("scissors");
-const userScoreElement = document.getElementById("user-score");
-const computerScoreElement = document.getElementById("computer-score");
-const playerChoiceElement = document.getElementById("player-choice");
-const computerChoiceElement = document.getElementById("computer-choice");
-const gameResultElement = document.getElementById("game-result");
-const playAgainButton = document.getElementById("play-again");
-
-// Function to show pop-up message
-function showPopupMessage(message) {
-  const popupBox = document.createElement("div");
-  popupBox.setAttribute("id", "popup-box");
-  popupBox.textContent = message;
-
-  // Position the pop-up box to the right of the game box
-  const gameContainer = document.getElementById("game-container");
-  const gameRect = gameContainer.getBoundingClientRect();
-  const popupBoxLeft = gameRect.right + 20;
-  const popupBoxTop = gameRect.top;
-
-  popupBox.style.position = "absolute";
-  popupBox.style.left = `${popupBoxLeft}px`;
-  popupBox.style.top = `${popupBoxTop}px`;
-
-  document.body.appendChild(popupBox);
-
-  // Remove the pop-up box after 3 seconds
-  setTimeout(() => {
-    popupBox.remove();
-  }, 3000);
-}
-
-// Function to get computer's choice
-function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return choices[randomIndex];
-}
-
-// Function to update the scores on the page
-function updateScore() {
-  userScoreElement.textContent = `User: ${userScore}`;
+// Function to update the scoreboard
+function updateScoreboard() {
+  const playerScoreElement = document.querySelector(".player-score");
+  const computerScoreElement = document.querySelector(".computer-score");
+  playerScoreElement.textContent = `Player: ${playerScore}`;
   computerScoreElement.textContent = `Computer: ${computerScore}`;
 }
 
-// Function to convert the choice text to the corresponding icon
-function getChoiceIcon(choice) {
-  if (choice === "rock") {
-    return '<i class="fas fa-hand-rock"></i>';
-  } else if (choice === "paper") {
-    return '<i class="fas fa-hand-paper"></i>';
-  } else if (choice === "scissors") {
-    return '<i class="fas fa-hand-scissors"></i>';
-  }
+// Function to display the round result message
+function displayRoundResultMessage(roundResult) {
+  const roundResultElement = document.getElementById("round-result");
+  roundResultElement.textContent = roundResult;
 }
 
-// Function to check the game result and update scores
-function checkResult(playerChoice) {
-  const computerChoice = getComputerChoice();
-  const playerChoiceIcon = getChoiceIcon(playerChoice);
-  const computerChoiceIcon = getChoiceIcon(computerChoice);
+// Function to display the end game message and the "Play Again?" button
+function displayEndGameMessage(message) {
+  const endGameContainer = document.getElementById("end-game");
+  const playAgainButton = document.createElement("button");
+  playAgainButton.textContent = "Play Again?";
+  playAgainButton.addEventListener("click", resetGame);
 
-  playerChoiceElement.innerHTML = `Player chose: ${playerChoiceIcon}`;
-  computerChoiceElement.innerHTML = `Computer chose: ${computerChoiceIcon}`;
+  endGameContainer.innerHTML = "";
+  endGameContainer.appendChild(document.createTextNode(message));
+  endGameContainer.appendChild(document.createElement("br"));
+  endGameContainer.appendChild(playAgainButton);
+}
 
+// Function to check the winner of the round
+function checkRoundWinner(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
-    gameResultElement.textContent = "It's a tie!";
-    showPopupMessage("It's a tie!");
+    return "It's a tie!";
   } else if (
     (playerChoice === "rock" && computerChoice === "scissors") ||
     (playerChoice === "paper" && computerChoice === "rock") ||
     (playerChoice === "scissors" && computerChoice === "paper")
   ) {
-    userScore++;
-    gameResultElement.textContent = "You win!";
+    playerScore++;
+    return "Player wins!";
   } else {
     computerScore++;
-    gameResultElement.textContent = "Computer wins!";
-  }
-
-  updateScore();
-
-  // Check if it's best 2 out of 3
-  if (userScore === 1 && computerScore === 1) {
-    showPopupMessage("Best 2 out of 3!");
-  }
-
-  // Check if one player has won 2 games
-  if (userScore === 2 || computerScore === 2) {
-    if (userScore === 2) {
-      showPopupMessage("Congratulations! You won 2 out of 3 games!");
-    } else {
-      showPopupMessage("Computer won 2 out of 3 games!");
-    }
-    // Disable the choice buttons and show "Play Again" button
-    rockButton.disabled = true;
-    paperButton.disabled = true;
-    scissorsButton.disabled = true;
-    playAgainButton.style.display = "block";
+    return "Computer wins!";
   }
 }
 
-// Event listeners for choice buttons
-rockButton.addEventListener("click", () => {
-  checkResult("rock");
-});
+// Function to handle player's choice and initiate the game logic
+function playGame(playerChoice) {
+  // Get the player's and computer's choice elements
+  const playerChoiceElement = document.getElementById("player-choice");
+  const computerChoiceElement = document.getElementById("computer-choice");
 
-paperButton.addEventListener("click", () => {
-  checkResult("paper");
-});
+  // Update the choice elements with the chosen icons
+  playerChoiceElement.innerHTML = `<i class="fas fa-hand-${playerChoice}"></i>`;
+  const computerChoice = getRandomChoice();
+  computerChoiceElement.innerHTML = `<i class="fas fa-hand-${computerChoice}"></i>`;
 
-scissorsButton.addEventListener("click", () => {
-  checkResult("scissors");
-});
+  if (currentRound === 0) {
+    // First round, display round result message
+    const roundResult = checkRoundWinner(playerChoice, computerChoice);
+    displayRoundResultMessage(roundResult);
+    currentRound++;
+  } else {
+    // Subsequent rounds, check if game should end
+    const roundResult = checkRoundWinner(playerChoice, computerChoice);
+    if (roundResult !== "It's a tie!") {
+      displayRoundResultMessage(roundResult);
+    }
 
-// Event listener for "Play Again" button
-playAgainButton.addEventListener("click", () => {
-  userScore = 0;
+    if (playerScore === 1 && computerScore === 1) {
+      // Game tied, next win takes all
+      displayRoundResultMessage("Looks like it's a tie, next win takes all");
+    } else if (playerScore === 2) {
+      // Player wins 2 out of 3 games
+      displayEndGameMessage("Congratulations! You won 2 out of 3 games!");
+    } else if (computerScore === 2) {
+      // Computer wins 2 out of 3 games
+      displayEndGameMessage(
+        "Oh no! Computer won 2 out of 3 games! Better luck next time!"
+      );
+    } else {
+      currentRound++;
+      if (currentRound <= totalRounds) {
+        // Display round result message
+        displayRoundResultMessage(
+          roundResult === "It's a tie!"
+            ? roundResult
+            : `Round ${currentRound} goes to ${roundResult}`
+        );
+      }
+    }
+  }
+
+  // Update the scoreboard
+  updateScoreboard();
+}
+
+// Function to reset the game
+function resetGame() {
+  playerScore = 0;
   computerScore = 0;
-  updateScore();
-  playerChoiceElement.textContent = "Player chose: ";
-  computerChoiceElement.textContent = "Computer chose: ";
-  gameResultElement.textContent = "";
-  rockButton.disabled = false;
-  paperButton.disabled = false;
-  scissorsButton.disabled = false;
-  playAgainButton.style.display = "none";
-});
+  currentRound = 0;
+
+  // Reset choices and round result on the screen
+  const playerChoiceElement = document.getElementById("player-choice");
+  const computerChoiceElement = document.getElementById("computer-choice");
+  const roundResultElement = document.getElementById("round-result");
+  playerChoiceElement.textContent = "";
+  computerChoiceElement.textContent = "";
+  roundResultElement.textContent = "";
+
+  // Reset the scoreboard
+  updateScoreboard();
+
+  // Clear the end game message and "Play Again?" button
+  const endGameContainer = document.getElementById("end-game");
+  endGameContainer.innerHTML = "";
+}
+
+// Helper function to generate a random choice for the computer
+function getRandomChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
